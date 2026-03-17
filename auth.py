8f252d8866f0d40561852a99f2411b3fcd952712
ayesha00000 <darazayesha@gmail.com>
2026-03-17 14:05:59 +0500

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,6 +1,4 @@
-def login(email, password, otp=None):
-    if "@" not in email:
-        return False
-    if email == "admin@example.com" and password == "1234":
+def login(username, password):
+    if username == "admin" and password == "1234":
         return True
     return False
