f4b635ad396d569b74685985e3738d9542f46566
ayesha00000 <darazayesha@gmail.com>
2026-03-26 15:52:51 +0500

    Update service.py

--- service.py
+++ service.py
@@ -1,4 +1,6 @@
-from auth import login
-
-def authenticate():
-    return login("admin", "1234")
+def get_all_users(users):
+    result = []
+    for user in users:
+        data = db.get_data(user.id)
+        result.append(data)
+    return result
