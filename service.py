d21c1dfcbc3461952c782c36c51643ce1ba83bba
ayesha00000 <darazayesha@gmail.com>
2026-03-26 15:12:50 +0500

    Update service.py

--- service.py
+++ service.py
@@ -1,4 +1,3 @@
-from auth import login
-
-def authenticate():
-    return login("admin", "1234")
+def get_user(user_input):
+    query = "SELECT * FROM users WHERE id = " + user_input
+    return db.execute(query)
